ad9329da357508b29f7c1f9622c5e8012ec802aa
fatinaelomar <fatina-el-omar@hotmail.co.uk>
2025-09-24 22:19:10 +0300

    fix run

--- Office.Application/Services/ChatService.cs
+++ Office.Application/Services/ChatService.cs
@@ -1,3 +1,4 @@
+using Office.Infrastructure.Repositories;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
--- Office.Infrastructure/Repositories/ChatRepository.cs
+++ Office.Infrastructure/Repositories/ChatRepository.cs
@@ -1,17 +1,35 @@
 using System.Threading.Tasks;
-using Office.Data.Entities;
-using Office.Data.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Office.Data.Entities;
 using Office.Infrastructure.Data;
 
-namespace Office.Infrastructure.Repositories {
-  public class ChatRepository : GenericRepository<AlChat> {
-    private readonly ApplicationDbContext _app;
-    public ChatRepository(ApplicationDbContext ctx) : base(ctx) { _app = ctx; }
-    public async Task<AlChat> AddMessageAsync(AlChat chat) {
-      await _app.AlChats.AddAsync(chat);
-      await _app.SaveChangesAsync();
-      return chat;
+namespace Office.Infrastructure.Repositories
+{
+    // Make sure this is public
+    public class ChatRepository : GenericRepository<AlChat>
+    {
+        private readonly ApplicationDbContext _app;
+
+        public ChatRepository(ApplicationDbContext ctx) : base(ctx)
+        {
+            _app = ctx;
+        }
+
+        // Adds a chat message and returns it
+        public async Task<AlChat> AddMessageAsync(AlChat chat)
+        {
+            if (chat == null)
+                throw new ArgumentNullException(nameof(chat));
+
+            await _app.AlChats.AddAsync(chat);
+            await _app.SaveChangesAsync();
+            return chat;
+        }
+
+        // Optional: get a chat by ID safely
+        public async Task<AlChat?> GetByIdAsync(int id)
+        {
+            return await _app.AlChats.FindAsync(id);
+        }
     }
-  }
 }
